857ace44b8652bf9679cf9ee2ffa12286ae526ba
makiGer <161993722+makiGer@users.noreply.github.com>
2024-05-12 23:18:49 +0800

    change something

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,17 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void StartButton()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
-    public void Tri()
+    public void Menu()
     {
         SceneManager.LoadSceneAsync(2);
     }
-
-    public void Para()
+    public void TriVector()
     {
         SceneManager.LoadSceneAsync(3);
+    }
+    public void ParaAddition()
+    {
+        SceneManager.LoadSceneAsync(4);
+    }
+    public void VecSub()
+    {
+        SceneManager.LoadSceneAsync(5);
+    }
+    public void VecSubAns()
+    {
+        SceneManager.LoadSceneAsync(6);
     }
 }
